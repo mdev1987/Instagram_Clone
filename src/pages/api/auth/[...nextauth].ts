d7977b6fccef9bbd356f8b7ad6db20d6108539b2
mdev1987 <m.developer66@gmail.com>
2023-03-17 01:18:47 +0330

    Install nex-auth and firebase and initialize firebase

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import nextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default nextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+});
