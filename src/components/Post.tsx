7c54cc4f2351b79c75e6a6c6fba80b3b4ce26c0d
mdev1987 <m.developer66@gmail.com>
2023-03-16 04:36:06 +0330

    Create the header and image section of the Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
 import { PostType } from "./Posts";
 
-export default function Post({ profile, image, caption }: PostType) {
+export default function Post({ profile, image, username, caption }: PostType) {
     return (
-        <div>
-            <Image src={image} width={500} height={500} alt="post-image" />
-            <p>{caption}</p>
+        <div className="bg-white my-7 border rounded-md">
+            <div className="flex items-center p-5">
+                <Image className="h-12 w-12 rounded-full object-cover p-1 border mr-3"
+                    src={profile} width={150} height={150} alt={username} />
+                <p className="font-bold flex-1">{username}</p>
+                <EllipsisVerticalIcon className="h-5" />
+            </div>
+            <div>
+                <Image src={image}
+                    className="object-cover w-full"
+                    width={500} height={500}
+                    alt={caption} />
+                <p>{caption}</p>
+            </div>
+
         </div>
     )
 }
