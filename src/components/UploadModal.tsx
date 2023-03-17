f5cd3870207010f6e80983c3beed4d13047a061b
mdev1987 <m.developer66@gmail.com>
2023-03-17 06:20:15 +0330

    Install react-modal and modify UploadModal component

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -1,17 +1,23 @@
 import { modalState } from "@/atom/modalAtom"
+import { CameraIcon } from "@heroicons/react/24/outline";
 import { useRecoilState } from "recoil"
+import Modal from 'react-modal'
 
 export default function UploadModal() {
     const [open, setOpen] = useRecoilState(modalState);
     return (
-        <>
-            <h2>Create Post</h2>
-            {open &&
-                <>
-                    <p>The Modal is open</p>
-                    <button onClick={() => setOpen(false)}>Close</button>
-                </>
-            }
-        </>
+        <Modal className="max-w-lg w-[90%] h-[300px] 
+        fixed top-56 bg-white rounded-sm
+        border-1 shadow-md        
+        left-[50%] 
+        focus:outline-none
+        translate-x-[-50%]"
+            shouldCloseOnOverlayClick={false}
+            isOpen={open}
+            onRequestClose={() => setOpen(false)} >
+            <div className="flex flex-col justify-center items-center h-[100%]">
+                <p>Modal</p>
+            </div>
+        </Modal >
     )
 }
